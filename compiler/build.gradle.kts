a76c834d69123171f03b7809b8829e0d6b5bb80b
Masatoshi Kubode <swi.masatoshi@gmail.com>
2018-10-25 23:38:23 +0900

    feat: Introduce KotlinPoet

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 dependencies {
     implementation(project(":core"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("com.squareup:kotlinpoet:1.0.0-RC1")
     testImplementation("junit:junit:4.12")
 }
 
