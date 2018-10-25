9caa9651ab88e934d6cb0a8832da33989069bb49
Masatoshi Kubode <swi.masatoshi@gmail.com>
2018-10-25 23:59:49 +0900

    feat: Introduce AutoService

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -3,11 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 dependencies {
     implementation(project(":core"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("com.google.auto.service:auto-service:1.0-rc4")
+    kapt("com.google.auto.service:auto-service:1.0-rc4")
     implementation("com.squareup:kotlinpoet:1.0.0-RC1")
     testImplementation("junit:junit:4.12")
 }
--- compiler/src/main/java/com/github/kubode/reaktor/compiler/ReaktorAnnotationProcessor.kt
+++ compiler/src/main/java/com/github/kubode/reaktor/compiler/ReaktorAnnotationProcessor.kt
@@ -1,12 +1,15 @@
 package com.github.kubode.reaktor.compiler
 
+import com.google.auto.service.AutoService
 import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.annotation.processing.SupportedAnnotationTypes
 import javax.annotation.processing.SupportedSourceVersion
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 
+@AutoService(Processor::class)
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 @SupportedAnnotationTypes("org.kotlin.annotationProcessor.TestAnnotation")
 class ReaktorAnnotationProcessor : AbstractProcessor() {
