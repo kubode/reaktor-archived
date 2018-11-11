c0a0293556c43420ac6ba0b6edf698f2f666a912
Masatoshi Kubode <swi.masatoshi@gmail.com>
2018-11-11 23:03:34 +0900

    WIP

--- core/src/main/java/com/github/kubode/reaktor/Reaktor.kt
+++ core/src/main/java/com/github/kubode/reaktor/Reaktor.kt
@@ -1,11 +1,58 @@
 package com.github.kubode.reaktor
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 annotation class TestAnnotation
 
-interface Reactor<ActionT, MutationT, StateT> {
-    val initialState: StateT
-    suspend fun mutate(action: ActionT): ReceiveChannel<MutationT>
-    fun reduce(state: StateT, mutation: MutationT): StateT
+fun debug(message: String) =
+    println("[${DateTimeFormatter.ofPattern("HH:mm:ss.SSS").format(LocalDateTime.now())}] [${Thread.currentThread().name}] $message")
+
+abstract class Reactor<ActionT, MutationT, StateT>(
+    initialState: StateT,
+    override val coroutineContext: CoroutineContext = EmptyCoroutineContext
+) : CoroutineScope {
+
+    object NoAction
+
+    private val _action = Channel<ActionT>()
+    private val _state = Channel<StateT>()
+
+    val action: SendChannel<ActionT> = _action
+    val state: ReceiveChannel<StateT> = _state
+
+    var currentState: StateT = initialState
+        private set(value) {
+            field = value
+            _state.offer(value)
+        }
+
+    init {
+        _state.offer(initialState)
+        launch {
+            _action.consumeEach { action ->
+                launch {
+                    mutate(action).consumeEach { mutation ->
+                        currentState = reduce(currentState, mutation)
+                    }
+                }
+            }
+        }
+    }
+
+    open fun mutate(action: ActionT): ReceiveChannel<MutationT> {
+        return Channel()
+    }
+
+    open fun reduce(state: StateT, mutation: MutationT): StateT {
+        return state
+    }
 }
--- example/cli/src/main/java/com/github/kubode/reaktor/example/cli/Main.kt
+++ example/cli/src/main/java/com/github/kubode/reaktor/example/cli/Main.kt
@@ -4,20 +4,28 @@ package com.github.kubode.reaktor.example.cli
 
 import com.github.kubode.reaktor.Reactor
 import com.github.kubode.reaktor.TestAnnotation
-import kotlinx.coroutines.CoroutineScope
+import com.github.kubode.reaktor.debug
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.flatMap
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.coroutines.coroutineContext
+import kotlinx.coroutines.yield
 
 @TestAnnotation
-class MyReactor : Reactor<MyReactor.Action, MyReactor.Mutation, MyReactor.State> {
+class MyReactor : Reactor<MyReactor.Action, MyReactor.Mutation, MyReactor.State>(State()) {
+
     sealed class Action {
         object Increment : Action()
         object IncrementDelayed : Action()
+
+        override fun toString() = javaClass.simpleName
     }
 
     sealed class Mutation {
@@ -28,21 +36,23 @@ class MyReactor : Reactor<MyReactor.Action, MyReactor.Mutation, MyReactor.State>
         val number: Int = 0
     )
 
-    override val initialState = State()
-
     private var delayedJob: Job? = null
 
-    override suspend fun mutate(action: Action): ReceiveChannel<Mutation> {
-        return CoroutineScope(coroutineContext).produce {
-            when (action) {
-                is Action.Increment -> {
-                    send(Mutation.Increment)
-                }
-                is Action.IncrementDelayed -> {
-                    delayedJob?.cancel()
-                    delayedJob = CoroutineScope(coroutineContext).launch {
+    override fun mutate(action: Action) = produce {
+        debug("MyReactor.mutate $action")
+        when (action) {
+            is Action.Increment -> {
+                send(Mutation.Increment)
+            }
+            is Action.IncrementDelayed -> {
+                delayedJob?.cancel()
+                delayedJob = launch {
+                    try {
                         delay(1000)
                         send(Mutation.Increment)
+                    } catch (e: CancellationException) {
+                        debug("Cancel! ${e.message}")
+                        throw e
                     }
                 }
             }
@@ -50,6 +60,7 @@ class MyReactor : Reactor<MyReactor.Action, MyReactor.Mutation, MyReactor.State>
     }
 
     override fun reduce(state: State, mutation: Mutation): State {
+        debug("MyReactor.reduce $state, $mutation")
         return when (mutation) {
             is Mutation.Increment -> state.copy(
                 number = state.number + 1
@@ -58,7 +69,45 @@ class MyReactor : Reactor<MyReactor.Action, MyReactor.Mutation, MyReactor.State>
     }
 }
 
-fun main(vararg args: String) = runBlocking {
+fun main() = runBlocking {
+//    val channel = produce {
+//        repeat(10) {i -> send(i) }
+//    }
+//        .flatMap { i ->
+//            produce {
+//                repeat(i) { j -> send(i * 10 + j) }
+//            }
+//        }
+//    launch {
+//        channel.consumeEach { debug("consume: $it") }
+//    }
+//    return@runBlocking
+    debug("main start")
+    val reactor = MyReactor()
+    launch {
+        reactor.state.consumeEach { state ->
+            debug("main reactor.state.consumeEach $state")
+        }
+    }
+    debug("main send increment 1")
+    reactor.action.offer(MyReactor.Action.Increment)
     delay(1000)
-    println("completed")
+
+    debug("main send increment 2")
+    reactor.action.offer(MyReactor.Action.Increment)
+
+    debug("main send increment delayed 3")
+    reactor.action.offer(MyReactor.Action.IncrementDelayed)
+    delay(500)
+
+    debug("main send increment delayed 4")
+    reactor.action.offer(MyReactor.Action.IncrementDelayed)
+    delay(500)
+
+    debug("main send increment 5")
+    reactor.action.offer(MyReactor.Action.Increment)
+    delay(1000)
+
+    coroutineContext.cancelChildren()
+    debug("main end")
 }
