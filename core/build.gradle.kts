c98171bfee12d75273a27a49bc9ff4ab6001e423
Masatoshi Kubode <swi.masatoshi@gmail.com>
2018-11-14 23:47:14 +0900

    Introduce Spek

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,6 +10,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0")
     implementation("com.squareup:kotlinpoet:1.0.0-RC1")
     testImplementation("junit:junit:4.12")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.0-rc.1")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.0-rc.1")
+    testRuntimeOnly(kotlin("reflect"))
 }
 
 kotlin {
